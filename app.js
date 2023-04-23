1424e455ea51cfb1209494b6cc355ebbb34aa1f7
Maida <maida.maxime@gmail.com>
2023-04-23 21:03:00 +0200

    New logic update 1.1

--- app.js
+++ app.js
@@ -6,75 +6,54 @@ const afficherObjetAleatoire = () => {
   const beltPromise = fetch("data/belt.json").then((response) =>
     response.json()
   );
+  const glovesPromise = fetch("data/gloves.json").then((response) =>
+    response.json()
+  );
 
   // Attendre que les deux fichiers soient chargés
-  Promise.all([bootsPromise, beltPromise])
-    .then(([bootsData, beltData]) => {
+  Promise.all([bootsPromise, beltPromise, glovesPromise])
+    .then(([bootsData, beltData, glovesData]) => {
       // Récupérer les objets de type "Boots" et "Belt"
       const boots = bootsData.boots;
       const belts = beltData.belt;
+      const gloves = glovesData.gloves;
 
       // Concaténer les objets dans un seul tableau
-      const objects = boots.concat(belts);
+      const objects = boots.concat(belts, gloves);
 
       // Choisir un objet aléatoire
       const randomIndex = Math.floor(Math.random() * objects.length);
       const object = objects[randomIndex];
 
-      // Vérifier si l'objet est une paire de bottes ou une ceinture
-      let additionalImplicitString = "";
-      let extraString = "";
-      if (object.type === "Boots") {
-        const implicit = object.implicit[1];
-        if (
-          implicit.hasOwnProperty("min") &&
-          implicit.hasOwnProperty("max") &&
-          implicit.hasOwnProperty("label")
-        ) {
-          const minSpeed = implicit.min;
-          const maxSpeed = implicit.max;
-          const speed = Math.floor(
-            minSpeed + Math.random() * (maxSpeed - minSpeed + 1)
+      // Générer les implicites
+      const implicits = object.implicit
+        .map((implicit) => {
+          const value = Math.floor(
+            implicit.min + Math.random() * (implicit.max - implicit.min + 1)
           );
-          additionalImplicitString = `<p>${speed}${implicit.label}</p>`;
-        }
-        if (object.implicit.length > 2) {
-          const extra = object.implicit[2];
-          if (
-            extra.hasOwnProperty("min") &&
-            extra.hasOwnProperty("max") &&
-            extra.hasOwnProperty("label")
-          ) {
-            const minExtra = extra.min;
-            const maxExtra = extra.max;
-            const extraValue = Math.floor(
-              minExtra + Math.random() * (maxExtra - minExtra + 1)
-            );
-            extraString = `<p>${extraValue}${extra.label}</p>`;
-          }
-        }
-      }
+          return `${value}${implicit.label}`;
+        })
+        .join("</p><p>");
 
       // Créer une chaîne de caractères pour afficher l'objet
       const output = `<div class="object-card">
-                  <h2>${object.name}</h2>
-                  <p>Type: ${object.type}</p>
-                  <p>Implicits:</p>
-                  <p>${object.implicit[0]}</p>
-                  ${additionalImplicitString}
-                  ${extraString}
-                  <p>Requirements: Level ${object.requirements.level}</p>
-                </div>`;
+                        <h2>${object.name}</h2>
+                        <p>Type: ${object.type}</p>
+                        <p>Implicits:</p>
+                        <p>${implicits}</p>
+                        <p>Requires: Level ${object.requirements.level}</p>
+                      </div>`;
 
       // Afficher l'objet dans l'élément HTML avec la classe "test"
-      const testDiv = document.querySelector(".item");
-      testDiv.innerHTML = output;
+      const loot = document.querySelector(".item");
+      loot.innerHTML = output;
     })
     .catch((error) => {
       console.error("Une erreur est survenue", error);
     });
 };
 
-// Générer un objet aléatoire
-document.querySelector("input[type=button]").onclick = () =>
+// Appeler la fonction pour afficher un objet aléatoire
+document.getElementById("lootBtn").addEventListener("click", () => {
   afficherObjetAleatoire();
+});
